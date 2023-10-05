a55af2fc28f582a880266fa2b67fdbd730ed31ee
Srikanth L <code@srik.me>
2023-10-05 20:09:13 +0000

    Add logging using morgan

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ import Express from 'express';
 import router from './lib/index.js';
 import swaggerDocument from './openapi/swagger.json' assert { type: "json" };
 import swaggerUi from 'swagger-ui-express';
+import morgan from 'morgan';
 
 const app = Express();
 const port = process.env.PORT || 3000;
+const logging = process.env.LOGGING || false;
+
 
 var options = {
   customCss: '.swagger-ui .topbar { display: none }'
@@ -16,6 +19,10 @@ app.use('/openapi.json', Express.static('openapi/swagger.json'));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.use('/api/', router);
+
+if (logging === true) {
+  app.use(morgan(':remote-addr :method :url :response-time'));
+}
 
 app.get('/', function (req, res) {
   res.redirect('/api-docs');
