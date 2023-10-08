f517952df7be1800dedaf39468dd624942d14c0e
Srikanth L <srikanthlogic@users.noreply.github.com>
2023-10-08 21:05:18 +0530

    Add logger using morgan (#50)
    * Bump eslint from 8.49.0 to 8.50.0
    
    Bumps [eslint](https://github.com/eslint/eslint) from 8.49.0 to 8.50.0.
    - [Release notes](https://github.com/eslint/eslint/releases)
    - [Changelog](https://github.com/eslint/eslint/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/eslint/eslint/compare/v8.49.0...v8.50.0)
    
    ---
    updated-dependencies:
    - dependency-name: eslint
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add logging using morgan
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import Express from 'express';
 import router from './lib/index.js';
 import swaggerDocument from './openapi/swagger.json' assert { type: "json" };
 import swaggerUi from 'swagger-ui-express';
+import morgan from 'morgan';
 
 const app = Express();
 const port = process.env.PORT || 3000;
@@ -12,14 +13,18 @@ var options = {
   customCss: '.swagger-ui .topbar { display: none }'
 };
 
-app.use('/openapi.json', Express.static('openapi/swagger.json'));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
-app.use('/api/', router);
+if (process.env.LOGGING || false) {
+  console.log('Logging is enabled');
+  app.use(morgan('combined'));
+}
 
 app.get('/', function (req, res) {
   res.redirect('/api-docs');
 });
+
+app.use('/openapi.json', Express.static('openapi/swagger.json'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.use('/api/', router);
 
 app.listen(port, function () {
   console.log('Server started on port', port);
